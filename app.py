77fe54efea95abffcb6265b177cb0e3d398ff0d2
Bruce Doan <rgv151@gmail.com>
2016-06-27 22:23:04 +0700

    add cache

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask, request, jsonify, make_response
+from werkzeug.contrib.cache import MemcachedCache
 
 from readability.readability import Document
 import requests
 
 application = Flask(__name__)
+
+cache = MemcachedCache(['memcache:11211'])
 
 
 @application.route("/")
@@ -11,27 +14,28 @@ def hello():
     id = request.args.get('id', '')
     url = request.args.get('url', '')
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safa,ri/537.36',
-        'Referer': 'https://news.ycombinator.com/item?id=' + id,
-        'Origin': 'https://news.ycombinator.com/item?id=' + id
-    }
-    try:
-        ret = requests.get(url, headers=headers)
-
-        doc = Document(ret.content, url=url)
-
-        result = {
-            "url": url,
-            "title": doc.short_title(),
-            "summary": doc.summary(True)
+    result = cache.get(id)
+    if result is None:
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safa,ri/537.36',
+            'Referer': 'https://news.ycombinator.com/item?id=' + id,
+            'Origin': 'https://news.ycombinator.com/item?id=' + id
         }
-    except:
-        result = {
-            "url": url,
-            "title": "Error",
-            "summary": "<h3>Unable to fetch article's content!</h3>"
-        }
+        try:
+            ret = requests.get(url, headers=headers)
+            doc = Document(ret.content, url=url)
+            result = {
+                "url": url,
+                "title": doc.short_title(),
+                "summary": doc.summary(True)
+            }
+        except:
+            result = {
+                "url": url,
+                "title": "Error",
+                "summary": "<h3>Unable to fetch article's content!</h3>"
+            }
+        cache.set(id, result)
 
     resp = make_response(jsonify(result), 200)
     resp.headers['Access-Control-Allow-Origin'] = '*'
@@ -39,4 +43,4 @@ def hello():
     return resp
 
 if __name__ == "__main__":
-    application.run(debug=True, use_reloader=True)
+    application.run(debug=False, use_reloader=False)
